444f3614eec141ed06b0880db8486f61e873fb7f
rirefat <rirefat.official@gmail.com>
2025-04-15 04:39:03 +0600

    done upto hero ring part

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -4,10 +4,10 @@ const Header = () => {
   return (
     <div className="flex justify-center items-center fixed top-4 w-full">
       <nav className="fex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
-        <Link href="#" className="nav-item">Home</Link>
-        <Link href="#" className="nav-item">About</Link>
-        <Link href="#" className="nav-item">Projects</Link>
-        <Link href="#" className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">Contact</Link>
+        <Link href="/home" className="nav-item">Home</Link>
+        <Link href="/about" className="nav-item">About</Link>
+        <Link href="/projects" className="nav-item">Projects</Link>
+        <Link href="/contact" className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">Contact</Link>
       </nav>
     </div>
   );
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -2,10 +2,22 @@ import Image from 'next/image';
 import memojiImage from '@/assets/images/memoji-computer.png';
 import ArrowDown from '@/assets/icons/arrow-down.svg';
 import Available from '@/assets/images/available.gif';
+import grainImage from '@/assets/images/grain.jpg';
 
 const HeroSection = () => {
   return (
-    <div className='py-32'>
+    <div className='py-32 md:py-48 lg:py-60 relative z-0'>
+      <div className="absolute inset-0 -z-30 opacity-10"
+        style={{
+          backgroundImage: `url(${grainImage.src})`
+        }}
+      />
+
+      <div className="size-[620px] hero-ring" />
+      <div className="size-[820px] hero-ring" />
+      <div className="size-[1020px] hero-ring" />
+      <div className="size-[1220px] hero-ring" />
+
       <div className="container">
         <div className="flex flex-col items-center">
           <Image
@@ -13,7 +25,7 @@ const HeroSection = () => {
             alt='Person peeking from behind laptop'
             className='size-[100px]'
           />
-          <div className='bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex gap-4 rounded-xl'>
+          <div className='bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex gap-4 rounded-2xl'>
             <div className='flex justify-center items-center'>
               <Image
                 src={Available}
@@ -25,10 +37,12 @@ const HeroSection = () => {
           </div>
         </div>
 
-        <h1 className='font-serif text-3xl text-center mt-8 tracking-wide'>Building Exceptional User Experience</h1>
-        <p className='mt-4 text-center text-white/60 '>I specialize in transforming designs into functional, high-performing web applications. Let&apos;s discuss your next project.</p>
+        <div className='max-w-lg mx-auto'>
+          <h1 className='font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide'>Building Exceptional User Experience</h1>
+          <p className='mt-4 text-center text-white/60 md:text-lg'>I specialize in transforming designs into functional, high-performing web applications. Let&apos;s discuss your next project.</p>
+        </div>
 
-        <div className='flex flex-col items-center gap-3 mt-8'>
+        <div className='flex flex-col md:flex-row justify-center items-center gap-3 mt-8'>
           <button className='font-semibold inline-flex items-center gap-2 border border-white/15 rounded-xl px-6 h-12 hover:bg-white/10 hover:text-white transition duration-300'>
             <span>Explore My Works</span>
             <span>🔎</span>
